7b556744dc98520a242806ac66a5e9d57d8caa60
Geraint Luff <luffgd@gmail.com>
2013-12-20 02:49:45 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function convertSchema(obj) {
+	if (obj.properties) {
+		var required = Array.isArray(obj.required) ? obj.required : [];
+		for (var key in obj.properties) {
+			var subSchema = obj.properties[key];
+			if (subSchema && typeof subSchema.required === 'boolean' && subSchema.required) {
+				required.push(key);
+			}
+		}
+		if (required.length) {
+			obj.required = required;
+		}
+	}
+	return obj;
+}
+
+module.exports = convertSchema;
