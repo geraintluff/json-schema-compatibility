f6752e2a5e7c3c8b91a8de4e3961eccf58f9aa46
Geraint Luff <luffgd@gmail.com>
2013-12-20 03:13:03 +0000

    divisiblyBy -> multipleOf

--- main.js
+++ main.js
@@ -1,27 +1,7 @@
 var JsonSchemaCompatability = (function () {
 
 	function convertSchema(obj) {
-		if (obj.properties) {
-			var required = Array.isArray(obj.required) ? obj.required : [];
-			for (var key in obj.properties) {
-				var subSchema = obj.properties[key];
-				if (subSchema && typeof subSchema.required === 'boolean' && subSchema.required) {
-					required.push(key);
-				}
-			}
-			if (required.length) {
-				obj.required = required;
-			}
-		}
-		
-		if (obj.dependencies) {
-			for (var key in obj.dependencies) {
-				if (typeof obj.dependencies[key] === 'string') {
-					obj.dependencies[key] = [obj.dependencies[key]];
-				}
-			}
-		}
-		
+		// Old-style "type"
 		if (obj.type && typeof obj.type !== 'string') {
 			var needsReplacement = false;
 			var anyOf = [];
@@ -39,6 +19,34 @@ var JsonSchemaCompatability = (function () {
 				delete obj.type;
 			}
 		}
+
+		// Object concerns
+		if (obj.properties) {
+			var required = Array.isArray(obj.required) ? obj.required : [];
+			for (var key in obj.properties) {
+				var subSchema = obj.properties[key];
+				if (subSchema && typeof subSchema.required === 'boolean' && subSchema.required) {
+					required.push(key);
+				}
+			}
+			if (required.length) {
+				obj.required = required;
+			}
+		}
+		if (obj.dependencies) {
+			for (var key in obj.dependencies) {
+				if (typeof obj.dependencies[key] === 'string') {
+					obj.dependencies[key] = [obj.dependencies[key]];
+				}
+			}
+		}
+		
+		// Numeric concerns
+		if (typeof obj.divisibleBy !== 'undefined') {
+			obj.multipleOf = obj.divisibleBy;
+			delete obj.divisibleBy;
+		}
+		
 		return obj;
 	}
 
